4a4410504ca2bb6a7cd14991e3cb92e3daa32ca2
Matin Wasker <pinkyandawa@gmail.com>
2025-08-22 11:12:48 +0700

    Add files via upload

---
+++ AboutView.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <h2>Pichayatida J. 67710722</h2>
+    <h2>ไป! บ้านนอก</h2>
+    <h3>Infromtion Technology</h3>
+  </div>
+</template>
